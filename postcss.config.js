ccf68fdec7d2a6edfbc8c163d38a6f5354ce5ce4
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-05-27 14:12:51 +0100

    adds postcss config

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-css-variables': {},
+    'postcss-conditionals': {},
+    'postcss-custom-media': {},
+    'css-mqpacker': {},
+    'perfectionist': {
+      format: 'compact',
+      trimTrailingZeros: false
+    },
+    'autoprefixer': {}
+  }
+};
